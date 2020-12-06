91bc1fc35e4f16cdd0cd383514fbd4351ffcc584
ngoctho <ngoctho.t1k54@gmail.com>
2020-12-06 17:03:22 +0700

    action bar

--- app/src/main/java/com/example/gmail/MainActivity.java
+++ app/src/main/java/com/example/gmail/MainActivity.java
@@ -1,40 +1,108 @@
 package com.example.gmail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.example.gmail.adapters.EmailBaseAdapter;
-import com.example.gmail.models.EmailItemModel;
+import com.example.gmail.models.Email;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.bloco.faker.Faker;
+
 public class MainActivity extends AppCompatActivity {
 
-    List<EmailItemModel> items;
-
-    String[] logos = {
-            "A", "B", "C", "D", "E", "F", "G", "H", "K", "L"
-    };
-
-    String[] names = {
-            "AGDFG", "BSDFGDGS", "CSDGDFG", "DDFGDFG", "EGDFGD", "FFDGDG", "GFDGDF", "HEWRWER", "KSDTRT", "LDFGDGDF"
-    };
+    List<Email> items;
+    SearchView txtSearchValue;
+    EmailBaseAdapter adapter;
+    boolean filterOnOFF = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        items = new ArrayList<>();
-        for (int i = 0; i < 10; i++)
-            items.add(new EmailItemModel(logos[i], names[i], "10:3" + i +" AM", "i write this letter to confirm something about...", true));
 
-        EmailBaseAdapter adapter = new EmailBaseAdapter(this, items);
+        Faker faker = new Faker();
+        items = new ArrayList<>();
+        for (int i = 0; i < 30; i++)
+            items.add(new Email(i, faker.name.firstName(), faker.company.name(), faker.lorem.paragraph(30), i + ":00", null, faker.bool.bool(), faker.bool.bool(), getRandomMaterialColor("400")));
+
+        items =  new ArrayList<>(items);
+
+        adapter = new EmailBaseAdapter(this, items);
 
         ListView listView = findViewById(R.id.list_mail);
         listView.setAdapter(adapter);
+    }
+
+    private int getRandomMaterialColor(String typeColor) {
+        int returnColor = Color.GRAY;
+        int arrayId = getResources().getIdentifier("mdcolor_" + typeColor, "array", getPackageName());
+
+        if (arrayId != 0) {
+            TypedArray colors = getResources().obtainTypedArray(arrayId);
+            int index = (int) (Math.random() * colors.length());
+            returnColor = colors.getColor(index, Color.GRAY);
+            colors.recycle();
+        }
+        return returnColor;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.setFilerType(EmailBaseAdapter.FILTER_TYPE_SEARCH);
+                adapter.getFilter().filter(newText);
+
+                return true;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_favorite) {
+            adapter.setFilerType(EmailBaseAdapter.FILTER_TYPE_IMPORTANT);
+
+            if (filterOnOFF) {
+                adapter.getFilter().filter(null);
+            } else {
+                adapter.getFilter().filter("1");
+            }
+
+            filterOnOFF = !filterOnOFF;
+
+            return true;
+        } else if (id == R.id.action_search) {
+            return true;
+        }
+
+        return false;
     }
 }
--- app/src/main/java/com/example/gmail/adapters/EmailBaseAdapter.java
+++ app/src/main/java/com/example/gmail/adapters/EmailBaseAdapter.java
@@ -1,35 +1,45 @@
 package com.example.gmail.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.gmail.R;
-import com.example.gmail.models.EmailItemModel;
+import com.example.gmail.models.Email;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class EmailBaseAdapter extends BaseAdapter {
+public class EmailBaseAdapter extends BaseAdapter implements Filterable {
     Context context;
-    List<EmailItemModel> items;
+    List<Email> items;
+    List<Email> itemListFiltered;
+    int filerType = 1;
+    public static final int FILTER_TYPE_IMPORTANT = 1;
+    public static final int FILTER_TYPE_SEARCH = 2;
 
-    public EmailBaseAdapter(Context context, List<EmailItemModel> items) {
+    public EmailBaseAdapter(Context context, List<Email> items) {
         this.context = context;
         this.items = items;
+        this.itemListFiltered = items;
     }
 
     @Override
     public int getCount() {
-        return items.size();
+        return itemListFiltered.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return items.get(position);
+        return itemListFiltered.get(position);
     }
 
     @Override
@@ -42,35 +52,45 @@ public class EmailBaseAdapter extends BaseAdapter {
         ViewHolder viewHolder;
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.list_row_email, parent, false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.email_list_row, parent, false);
+
             viewHolder = new ViewHolder();
 
-            viewHolder.logo = convertView.findViewById(R.id.logo);
-            viewHolder.name = convertView.findViewById(R.id.name);
-            viewHolder.description = convertView.findViewById(R.id.description);
-            viewHolder.time = convertView.findViewById(R.id.time);
-            viewHolder.star = convertView.findViewById(R.id.btn_star);
+            viewHolder.from = convertView.findViewById(R.id.from);
+            viewHolder.subject = convertView.findViewById(R.id.txt_primary);
+            viewHolder.message = convertView.findViewById(R.id.txt_secondary);
+            viewHolder.iconText = convertView.findViewById(R.id.icon_text);
+            viewHolder.timestamp = convertView.findViewById(R.id.timestamp);
+            viewHolder.iconImp = convertView.findViewById(R.id.icon_star);
+            viewHolder.imgProfile = convertView.findViewById(R.id.icon_profile);
 
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
 
-            EmailItemModel item = items.get(position);
-            viewHolder.logo.setText(item.getLogo());
-            viewHolder.name.setText(item.getName());
-            viewHolder.time.setText(item.getTime());
-            viewHolder.description.setText(item.getDescription());
+            Email item = itemListFiltered.get(position);
+            // displaying text view data
+            viewHolder.from.setText(item.getFrom());
+            viewHolder.subject.setText(item.getSubject());
+            viewHolder.message.setText(item.getMessage());
+            viewHolder.timestamp.setText(item.getTimestamp());
 
-            if (item.isFavorite()) {
-                viewHolder.star.setImageResource(R.drawable.ic_baseline_star_48);
+            // displaying the first letter of From in icon text
+            viewHolder.iconText.setText(item.getFrom().substring(0, 1));
+            viewHolder.imgProfile.setImageResource(R.drawable.bg_circle);
+            viewHolder.imgProfile.setColorFilter(item.getColor());
+
+            if (item.isImportant()) {
+                viewHolder.iconImp.setImageResource(R.drawable.ic_baseline_star_rate_24);
             } else {
-                viewHolder.star.setImageResource(R.drawable.ic_baseline_star_border_48);
+                viewHolder.iconImp.setImageResource(R.drawable.ic_baseline_star_border_24);
             }
 
-            viewHolder.star.setOnClickListener(new View.OnClickListener() {
+
+            viewHolder.iconImp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    item.setFavorite(!item.isFavorite());
+                    item.setImportant(!item.isImportant());
                     notifyDataSetChanged();
                 }
             });
@@ -79,11 +99,81 @@ public class EmailBaseAdapter extends BaseAdapter {
         return convertView;
     }
 
+    @Override
+    public Filter getFilter() {
+        Filter importantFilter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint == null || constraint.length() == 0) {
+                    filterResults.count = items.size();
+                    filterResults.values = items;
+                } else {
+                    List<Email> resultsModel = new ArrayList<>();
+
+                    for (Email item : items) {
+                        if (item.isImportant()) {
+                            resultsModel.add(item);
+                        }
+                    }
+
+                    filterResults.count = resultsModel.size();
+                    filterResults.values = resultsModel;
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                itemListFiltered = (List<Email>) results.values;
+                EmailBaseAdapter.this.notifyDataSetChanged();
+            }
+        };
+
+        Filter searchFilter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint == null || constraint.length() == 0) {
+                    filterResults.count = items.size();
+                    filterResults.values = items;
+                } else {
+                    List<Email> resultsModel = new ArrayList<>();
+
+                    for (Email item : items) {
+                        if (item.getFrom().contains(constraint) || item.getSubject().contains(constraint)) {
+                            resultsModel.add(item);
+                        }
+                    }
+
+                    filterResults.count = resultsModel.size();
+                    filterResults.values = resultsModel;
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                itemListFiltered = (List<Email>) results.values;
+                EmailBaseAdapter.this.notifyDataSetChanged();
+            }
+        };
+
+        if (filerType == FILTER_TYPE_IMPORTANT)
+            return importantFilter;
+        else {
+            return searchFilter;
+        }
+    }
+
     private class ViewHolder {
-        TextView logo;
-        TextView name;
-        TextView time;
-        TextView description;
-        ImageButton star;
+        TextView from, subject, message, iconText, timestamp;
+        ImageView iconImp, imgProfile;
+    }
+
+    public void setFilerType(int filerType) {
+        this.filerType = filerType;
     }
 }
